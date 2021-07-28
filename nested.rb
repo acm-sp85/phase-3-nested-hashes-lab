2337bcbbf4cf126caf854b149300727368e10d69
alex <acontell@gmail.com>
2021-07-28 09:13:05 +0200

    Completed Assignment

--- nested.rb
+++ nested.rb
@@ -15,27 +15,28 @@ def hopper
         :languages => ["C"]
       }
     }
+    return programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
+  {
+    :grace_hopper => {
+      :known_for => "COBOL",
+      :languages => ["COBOL", "FORTRAN"]
+    },
+    :alan_kay => {
+      :known_for => "Object Orientation",
+      :languages => ["Smalltalk", "LISP"]
+    },
+    :dennis_ritchie => {
+      :known_for => "Unix",
+      :languages => ["C"]
     }
+  }
+  return programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    return programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -79,9 +81,17 @@ def adding_matz
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
-      }
+      },
+      
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby" ,
+          :languages=>["LISP","C"]
+          }
+        
     }
 
+
+    return programmer_hash
 end
 
 def changing_alan
@@ -103,7 +113,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = "GUI"
+return programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +136,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages] = programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+return programmer_hash
 end
